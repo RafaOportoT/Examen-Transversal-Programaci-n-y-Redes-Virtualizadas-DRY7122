3f385cd7fa72d2a84e616ae2e4ee27ee0720e12e
RafaOportoT <ra.oporto@duocuc.cl>
2025-07-10 19:43:28 -0400

    Create integrantes.py

---
+++ integrantes.py
@@ -0,0 +1,15 @@
+# integrantes.py
+
+# Lista con los nombres y apellidos de los integrantes del grupo
+integrantes = 
+[
+    "Rafael Oporto",
+    "Fernando Olea",
+    "Javier Pizarro",
+    "Sebastian Celis"
+]
+
+# Imprimir la lista
+print("Integrantes del grupo:")
+for persona in integrantes:
+    print("-", persona)
